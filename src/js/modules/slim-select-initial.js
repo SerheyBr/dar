c69cf68840431fccd95f44f1cdf5b45aedbe1b7d
SerheyBr <sergeydrilkov@mail.ru>
2025-10-06 18:21:12 +0300

    add hero section

--- src/js/modules/slim-select-initial.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  new SlimSelect({
-    select: ".my-form .wpcf7-select",
-    settings: {
-      showSearch: false,
-      placeholderText: "Выберите услугу",
-    },
-  });
-});
